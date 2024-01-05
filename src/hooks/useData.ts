07aa01dcd7669e45913c93f2eaba246ec5d97ee5
Tommy Nedow <tommy.nedow@gmail.com>
2024-01-05 11:28:15 -0500

    filtered game by genre

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import apiClients from "../services/api-clients";
-import { CanceledError } from "axios";
+import { AxiosRequestConfig, CanceledError } from "axios";
 
 
 interface FetchResponse<T>{
@@ -8,7 +8,7 @@ interface FetchResponse<T>{
     results: T[];
 }
 
-const useData = <T> (endpoint:string) => {
+const useData = <T> (endpoint:string, requestConfig?: AxiosRequestConfig, deps?: any[]) => {
     const [data, setData] = useState<T[]>([]);
     const [error, setError] = useState("");
     const [isLoading, setLoading] = useState(false);
@@ -18,7 +18,7 @@ const useData = <T> (endpoint:string) => {
   
       setLoading(true);
       apiClients
-        .get<FetchResponse<T>>(endpoint, {signal: controller.signal})
+        .get<FetchResponse<T>>(endpoint, {signal: controller.signal, ...requestConfig})
         .then((res) => {
           setData(res.data.results)
           setLoading(false);
@@ -30,7 +30,7 @@ const useData = <T> (endpoint:string) => {
         });
   
         return () => controller.abort();
-    }, []); // Empty dependency array to run the effect only once on mount
+    }, deps ? [...deps]: []); // Empty dependency array to run the effect only once on mount
   
     return { data, error, isLoading };
   };
